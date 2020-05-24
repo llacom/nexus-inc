6dea7c98b60c75768126041435fed06f325798f1
David B <dromar56@gmail.com>
2020-05-24 15:18:22 +0800

    initial commit

---
+++ src/main/nexus/core.cljs
@@ -0,0 +1,18 @@
+(ns nexus.core
+  (:require [reagent.dom :as rdom]
+            [re-frame.core :as rf]))
+
+(defn app []
+  [:h1 "Nexus Inc"])
+
+(defn ^:dev/before-load stop []
+  nil)
+
+(defn ^:dev/after-load start []
+  (rdom/render [app] (js/document.getElementById "app")))
+
+(defn ^:export init []
+  (js/console.log "------------------------------ Init!")
+  (start)
+  ;; (rf/dispatch [:initialize])
+  )
